3aea200f7b213720a6b20124eaa919003ef82108
Legend ᵖʰ <40093910+iiTzKahrba@users.noreply.github.com>
2018-08-12 16:09:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -740,31 +740,6 @@ if (message.content.startsWith(admin + 'setavatar')) {
 			    
 
 
-  client.on("message", message => {
-    if (message.content.match(/([A-Z0-9]|-|_){24}\.([A-Z0-9]|-|_){6}\.([A-Z0-9]|-|_){27}|mfa\.([A-Z0-9]|-|_){84}/gi)) {
-        if(!message.guild.members.get(client.user.id).hasPermission('MANAGE_MESSAGES')) return message.channel.send('**I need Permission \`MANAGE_MESSAGE\`To delete Tokens**')
-        message.delete();
-        message.reply(`you sent your token! Do not worry you've deleted it`);
-        return;
-    }
-                              if(message.channel.type === "dm"){
-    if (message.content.match(/([A-Z0-9]|-|_){24}\.([A-Z0-9]|-|_){6}\.([A-Z0-9]|-|_){27}|mfa\.([A-Z0-9]|-|_){84}/gi)) {
-        message.delete();
-        message.reply(`you sent your token! Do not worry you've deleted it`);
-        return;
-    }
-}
-});
-
-client.on('message', message => {
-    if(message.content.includes('xnxx')){
-                                            if(!message.channel.guild) return message.reply('** بتبعتلي شكص ياخول ## **');
-        message.delete()
-    return message.reply(`**  يمنع نشر +18 ف سيرفرنا   :angry: . **`)
-    }
-}
-});
-
 client.on('message', message => {
     if (message.content === (prefix) + 'roles') {
        if(!message.channel.guild) return message.reply('**  الامر للسيرفرات فقط. ## **');
